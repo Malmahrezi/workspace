e0ecf0e45e38769c0e5e7084948be77b4c4436a4
Malmahrezi <Malmahrezi@users.noreply.github.com>
2022-07-23 17:58:35 +0000

    automated commit by check50 [check50=True]

--- mario.py
+++ mario.py
@@ -5,7 +5,7 @@ def main():
     while True:
         height = get_int("Height: ")
         width = height
-        if height >= 0 and height <= 23:
+        if height >= 1 and height <= 8:
             break
 
     for i in range(1, height + 1):
