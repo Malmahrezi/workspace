8825c00a046ca3c5038ed3ad6684d133b31151ad
Malmahrezi <Malmahrezi@users.noreply.github.com>
2022-07-24 13:28:43 +0000

    automated commit by check50 [check50=True]

--- dna.py
+++ dna.py
@@ -1,63 +1,36 @@
-from sys import argv
-import csv
-
+from csv import reader, DictReader
+from sys import argv, exit
 def main():
-    sequenceRepeats = {}
     if len(argv) != 3:
-        print("Usage: python dna.py data.csv. sequence.txt")
-        return 1
-    checkSTRS = []
-    with open(argv[1], "r") as strFile:
-        reader = csv.reader(strFile)
-        lineCounter = 0
-        for line in reader:
-            if lineCounter == 0:
-                for STR in line:
-                    if STR != 'name':
-                        checkSTRS.append(STR)
-                break
-            break
-    with open(argv[2], "r") as sequenceFile:
-        for line in sequenceFile:
-            sequence = line
-        for strepeat in checkSTRS:
-            sequenceRepeats[strepeat] = getRepeats(sequence, strepeat)
-    with open(argv[1], "r") as strFile:
-        reader = csv.DictReader(strFile)
-        matchedPerson = "No match"
-        for line in reader:
-            strsMatched = 0
-            for strepeat in checkSTRS:
-                if line[strepeat] == str(sequenceRepeats[strepeat]):
-                    strsMatched += 1
-            if strsMatched == len(checkSTRS):
-                matchedPerson = line['name']
-                break
-        print(matchedPerson)
-def getRepeats(sequence, strepeat):
-    strlength = len(strepeat)
-    baseCounter = 0
-    lastRepeatIndex = 0
-    conRepeats = 0
-    repeatList = []
-    while baseCounter <= len(sequence) - 1:
-        # Look at the baseCounter and X bases in front of it - where x is length of     STR to search for
-        check = sequence[baseCounter: baseCounter+strlength]
-        if check == strepeat:
-            if lastRepeatIndex == baseCounter - strlength:
-                conRepeats += 1
-            elif lastRepeatIndex != 0:
-                repeatList.append(conRepeats)
-                conRepeats = 0
-            else:
-                pass
-            lastRepeatIndex = baseCounter
-            baseCounter += strlength
-            if conRepeats == 0:
-                conRepeats += 1
-        else:
-            baseCounter += 1
-    repeatList.append(conRepeats)
-    return max(repeatList)
-if __name__ == "__main__":
-    main()
+        print("Usage: python dna.py data.csv sequence.txt")
+        exit(1)
+    db_path = argv[1]
+    seq_path = argv[2]
+    with open(db_path, "r") as csvfile:
+        reader = DictReader(csvfile)
+        dict_list = list(reader)
+    with open(seq_path, "r") as file:
+        sequence = file.read()
+    max_counts = []
+    for i in range(1, len(reader.fieldnames)):
+        STR = reader.fieldnames[i]
+        max_counts.append(0)
+        for j in range(len(sequence)):
+            STR_count = 0
+            if sequence[j:(j + len(STR))] == STR:
+                k = 0
+                while sequence[(j + k):(j + k + len(STR))] == STR:
+                    STR_count += 1
+                    k += len(STR)
+                if STR_count > max_counts[i - 1]:
+                    max_counts[i - 1] = STR_count
+    for i in range(len(dict_list)):
+        matches = 0
+        for j in range(1, len(reader.fieldnames)):
+            if int(max_counts[j - 1]) == int(dict_list[i]  [reader.fieldnames[j]]):
+                matches += 1
+            if matches == (len(reader.fieldnames) - 1):
+                print(dict_list[i]['name'])
+                exit(0)
+print("No match")
+main()
