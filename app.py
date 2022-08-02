aef5752eb3b93daa98ae0721340be7c4a7a709a1
Malmahrezi <Malmahrezi@users.noreply.github.com>
2022-08-02 08:02:32 +0000

    automated commit by check50 [check50=True]

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from cs50 import SQL
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from tempfile import mkdtemp
-from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
+
 from werkzeug.security import check_password_hash, generate_password_hash
 import datetime
 import math
@@ -281,6 +281,4 @@ def errorhandler(e):
     return apology(e.name, e.code)
 
 
-# Listen for errors
-for code in default_exceptions:
-    app.errorhandler(code)(errorhandler)
+
