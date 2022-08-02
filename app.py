8e140e6fe979f1d4e2d7d59e2e4e92e800094423
Malmahrezi <Malmahrezi@users.noreply.github.com>
2022-08-02 07:58:09 +0000

    automated commit by check50 [check50=True]

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.security import check_password_hash, generate_password_hash
+import datetime
+import math
 
 from helpers import apology, login_required, lookup, usd
 
