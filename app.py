a15af75dafb3bb2a5846125215258baffb4834ed
Malmahrezi <Malmahrezi@users.noreply.github.com>
2022-08-02 08:00:37 +0000

    automated commit by check50 [check50=True]

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from cs50 import SQL
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from tempfile import mkdtemp
+from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
 import datetime
 import math
