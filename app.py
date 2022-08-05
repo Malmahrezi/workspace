94de4e2b2677c8161f4c96157646e5d066cc5226
Malmahrezi <Malmahrezi@users.noreply.github.com>
2022-08-05 04:59:30 +0000

    automated commit by check50 [check50=True]

--- app.py
+++ app.py
@@ -263,7 +263,7 @@ def sell():
     if stock == None:
             return apology("Symbol Does Not Exist")
 
-    if shares < 0:
+    if shares <= 0:
             return apology("Share Not allowed")
 
     transaction_value = shares * stock["price"]
