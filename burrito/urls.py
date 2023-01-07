befc5332431c4023db36ba10f671d7bf7c122e9d
Malmahrezi <Malmahrezi@users.noreply.github.com>
2023-01-07 20:43:16 +0400

    automated commit by submit50

--- burrito/urls.py
+++ burrito/urls.py
@@ -1,4 +1,4 @@
-"""burrito URL Configuration
+"""burrito configuration urls
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.0/topics/http/urls/
 Examples:
